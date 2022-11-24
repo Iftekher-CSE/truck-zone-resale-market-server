7d209122a870d495cb9b0cbadb905659db2604a1
Iftekher-CSE <108381759+Iftekher-CSE@users.noreply.github.com>
2022-11-25 01:51:17 +0600

    product post api created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const { query } = require("express");
 require("dotenv").config();
 
 const app = express();
@@ -21,6 +22,36 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+        // Collections
+        const categoriesCollection = client.db("truckZone").collection("truckCategory");
+        const trucksCollection = client.db("truckZone").collection("allTrucks");
+
+        // get all truck cat
+        app.get("/truck-categories", async (req, res) => {
+            const result = await categoriesCollection.find({}).toArray();
+            res.send(result);
+        });
+
+        // get products based on cat
+        app.get("/trucks/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { catName: id };
+            const trucks = await trucksCollection.find(query).toArray();
+            res.send(trucks);
+        });
+
+        //get truck categories name
+        app.get("/categoryName", async (req, res) => {
+            const categoryName = await categoriesCollection.find({}).project({ catName: 1 }).toArray();
+            res.send(categoryName);
+        });
+
+        // post a truck to database
+        app.post("/trucks", async (req, res) => {
+            const truck = req.body;
+            const result = await trucksCollection.insertOne(truck);
+            res.send(result);
+        });
     } finally {
     }
 }
